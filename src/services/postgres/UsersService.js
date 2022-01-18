b9bd1ddbcb2a5c0d5c4e211407abc1b5bab56db7
tsaqifrazin1 <tsaqifgojek@gmail.com>
2022-01-18 17:37:04 +0800

    authentication implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -3,6 +3,7 @@ const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
   constructor() {
@@ -11,7 +12,6 @@ class UsersService {
 
   async addUser({ username, password, fullname }) {
     await this.verifyNewUsername(username);
-    console.log(username)
     const id = `user-${nanoid(16)}`;
     const hashedPassword = await bcrypt.hash(password, 10);
     const query = {
@@ -53,6 +53,28 @@ class UsersService {
     }
 
     return result.rows[0];
+  }
+  async verifyUserCredential(username, password){
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username]
+    }
+
+    const result = await this._pool.query(query);
+
+    if(!result.rows.length){
+      throw new AuthenticationError('Kredensial yang Anda berikan salah')
+    }
+
+    const { id, password: hashedPassword } = result.rows[0]
+
+    const match = await bcrypt.compare(password, hashedPassword)
+
+    if (!match){
+      throw new AuthenticationError('Kredensial yang Anda berikan salah')
+    }
+
+    return id
   }
 }
 
