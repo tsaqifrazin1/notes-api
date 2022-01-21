d8c5392d8cf780b1c80f2e52bcf4f99946037c96
tsaqifrazin1 <tsaqifgojek@gmail.com>
2022-01-22 00:04:58 +0800

    collaboration implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -64,7 +64,7 @@ class NotesHandler {
             const { id } = request.params;
             const { id: credentialId } = request.auth.credentials
 
-            await this._service.verifyNoteOwner(id, credentialId)    
+            await this._service.verifyNoteAccess(id, credentialId)    
             const note = await this._service.getNoteById(id);
             return {
                 status : 'success',
@@ -99,7 +99,7 @@ class NotesHandler {
           const { id } = request.params;
           const { id: credentialId } = request.auth.credentials
 
-          await this._service.verifyNoteOwner(id, credentialId)  
+          await this._service.verifyNoteAccess(id, credentialId)  
      
           await this._service.editNoteById(id, { title, body, tags });
      
