fe094a56aff3571da5463df61281f3fe8df4dcea
Tsaqif Razin <tsaqifrazin@gmail.com>
2022-02-19 17:26:03 +0800

    Deploy to heroku

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-underscore-dangle */
 const ClientError = require('../../exceptions/ClientError');
- 
+
 class AuthenticationsHandler {
   constructor(authenticationsService, usersService, tokenManager, validator) {
     this._authenticationsService = authenticationsService;
@@ -11,28 +12,28 @@ class AuthenticationsHandler {
     this.putAuthenticationHandler = this.putAuthenticationHandler.bind(this);
     this.deleteAuthenticationHandler = this.deleteAuthenticationHandler.bind(this);
   }
- 
+
   async postAuthenticationHandler(request, h) {
     try {
-        this._validator.validatePostAuthenticationPayload(request.payload);
+      this._validator.validatePostAuthenticationPayload(request.payload);
 
-        const { username, password } = request.payload
-        const id = await this._usersService.verifyUserCredential(username, password);
-        const accessToken = await this._tokenManager.generateAccessToken({ id })
-        const refreshToken = await this._tokenManager.generateRefreshToken({ id })
+      const { username, password } = request.payload;
+      const id = await this._usersService.verifyUserCredential(username, password);
+      const accessToken = await this._tokenManager.generateAccessToken({ id });
+      const refreshToken = await this._tokenManager.generateRefreshToken({ id });
 
-        await this._authenticationsService.addRefreshToken(refreshToken)
+      await this._authenticationsService.addRefreshToken(refreshToken);
 
-        const response = h.response({
-            status: 'success',
-            message: 'Authentication berhasil ditambahkan',
-            data: {
-                accessToken,
-                refreshToken,
-            },
-        })
-        response.code(201);
-        return response
+      const response = h.response({
+        status: 'success',
+        message: 'Authentication berhasil ditambahkan',
+        data: {
+          accessToken,
+          refreshToken,
+        },
+      });
+      response.code(201);
+      return response;
     } catch (error) {
       if (error instanceof ClientError) {
         const response = h.response({
@@ -42,7 +43,7 @@ class AuthenticationsHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
@@ -53,24 +54,25 @@ class AuthenticationsHandler {
       return response;
     }
   }
+
   async putAuthenticationHandler(request, h) {
     try {
-        this._validator.validatePutAuthenticationPayload(request.payload)
-        const { refreshToken } = request.payload
+      this._validator.validatePutAuthenticationPayload(request.payload);
+      const { refreshToken } = request.payload;
 
-        await this._authenticationsService.verifyRefreshToken(refreshToken)
+      await this._authenticationsService.verifyRefreshToken(refreshToken);
 
-        const { id } = this._tokenManager.verifyRefreshToken(refreshToken)
+      const { id } = this._tokenManager.verifyRefreshToken(refreshToken);
 
-        const accessToken = this._tokenManager.generateAccessToken({ id })
+      const accessToken = this._tokenManager.generateAccessToken({ id });
 
-        return {
-            status: 'success',
-            message: 'Access Token berhasil diperbarui',
-            data: {
-            accessToken,
-            },
-        }
+      return {
+        status: 'success',
+        message: 'Access Token berhasil diperbarui',
+        data: {
+          accessToken,
+        },
+      };
     } catch (error) {
       if (error instanceof ClientError) {
         const response = h.response({
@@ -80,7 +82,7 @@ class AuthenticationsHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
@@ -91,18 +93,19 @@ class AuthenticationsHandler {
       return response;
     }
   }
+
   async deleteAuthenticationHandler(request, h) {
     try {
-        this._validator.validateDeleteAuthenticationPayload(request.payload)
+      this._validator.validateDeleteAuthenticationPayload(request.payload);
 
-        const { refreshToken } = request.payload
-        await this._authenticationsService.verifyRefreshToken(refreshToken)
-        await this._authenticationsService.deleteRefreshToken(refreshToken)
+      const { refreshToken } = request.payload;
+      await this._authenticationsService.verifyRefreshToken(refreshToken);
+      await this._authenticationsService.deleteRefreshToken(refreshToken);
 
-        return {
-            status: 'success',
-            message: 'Refresh token berhasil dihapus',
-          };
+      return {
+        status: 'success',
+        message: 'Refresh token berhasil dihapus',
+      };
     } catch (error) {
       if (error instanceof ClientError) {
         const response = h.response({
@@ -112,7 +115,7 @@ class AuthenticationsHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
